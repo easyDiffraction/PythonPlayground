d726a5ab05393c85db86d8f1fb956bd420f4f490
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-01-16 18:39:02 +0100

    Attack of PEP3107

--- Utils/URDict.py
+++ Utils/URDict.py
@@ -1,9 +1,11 @@
+from typing import Union, Any, NoReturn, List
+
 from PySide2.QtWidgets import QUndoStack, QUndoCommand
 
 
 class URDict(dict):
     class _StackCommand(QUndoCommand):
-        def __init__(self, dictionary, key, value):
+        def __init__(self, dictionary: dict, key: str, value: Any):
             QUndoCommand.__init__(self)
             self._dictionary = dictionary
             self._key = key
@@ -17,7 +19,7 @@ class URDict(dict):
 
             self._new_value = value
 
-        def undo(self):
+        def undo(self) -> NoReturn:
             # self.setText("     undo command {} - {}:{} = ".format(self._dictionary, self._key, self._value))
             if self._old_value is None:
                 self.setText("     undo command {} - {}:{} = ".format(self._dictionary, self._key, self._new_value))
@@ -25,7 +27,7 @@ class URDict(dict):
             else:
                 self._dictionary.__realsetitem__(self._key, self._old_value)
 
-        def redo(self):
+        def redo(self) -> NoReturn:
             # self.setText("  do/redo command {} + {}:{} = ".format(self._dictionary, self._key, self._value))
             self._dictionary.__realsetitem__(self._key, self._new_value)
 
@@ -33,33 +35,33 @@ class URDict(dict):
         super().__init__(*args, **kwargs)
         self.__stack__ = QUndoStack()
 
-    def __setitem__(self, key, val):
+    def __setitem__(self, key: str, val: Any) -> NoReturn:
         self.__stack__.push(self._StackCommand(self, key, val))
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Union[str, List]) -> Any:
         if isinstance(key, list):
             return self.getByPath(key)
         return super().__getitem__(key)
 
-    def __realsetitem__(self, key, val):
+    def __realsetitem__(self, key: Union[str, List], val: Any):
         if isinstance(key, list):
             self.setByPath(key, val)
         else:
             super().__setitem__(key, val)
 
-    def undoText(self):
+    def undoText(self) -> NoReturn:
         return self.__stack__.undoText()
 
-    def redoText(self):
+    def redoText(self) -> NoReturn:
         return self.__stack__.redoText()
 
-    def undo(self):
+    def undo(self) -> NoReturn:
         self.__stack__.undo()
 
-    def redo(self):
+    def redo(self) -> NoReturn:
         self.__stack__.redo()
 
-    def getByPath(self, keys):
+    def getByPath(self, keys: List) -> Any:
         """Access a nested object in root by key sequence.
         We can't use reduce and operator"""
         item = self
@@ -70,6 +72,6 @@ class URDict(dict):
                 raise KeyError
         return item
 
-    def setByPath(self, keys, value):
+    def setByPath(self, keys: List, value: Any) -> NoReturn:
         """Get a value in a nested object in root by key sequence."""
         self.getByPath(keys[:-1])[keys[-1]] = value
