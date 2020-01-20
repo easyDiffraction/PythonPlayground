acc372cccd3f4bb80d98d8e9d1f23fd98c0165a0
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-01-20 16:05:28 +0100

    Alternative implementation of DictUndoRedoDecorator

---
+++ DictUndoRedoDecorator2/URDict.py
@@ -0,0 +1,140 @@
+from typing import Union, Any, NoReturn, List
+
+from PySide2.QtWidgets import QUndoStack, QUndoCommand
+
+
+class _EmptyCommand(QUndoCommand):
+    """
+    The _EmptyCommand class is the custom base class of all undoable commands
+    stored on a QUndoStack.
+    """
+
+    def __init__(self, dictionary: dict, key: str, value: Any):
+        QUndoCommand.__init__(self)
+        self._dictionary = dictionary
+        self._key = key
+        self._new_value = value
+        self._old_value = dictionary.getItem(key)
+
+
+class _AddItemCommand(_EmptyCommand):
+    """
+    The _AddItemCommand class implements a command to add a key-value pair to
+    the URDict-based dictionary.
+    """
+
+    def undo(self) -> NoReturn:
+        self.setText("Deleting: {}".format(self._key))
+        self._dictionary._realDelItem(self._key)
+
+    def redo(self) -> NoReturn:
+        self.setText("Adding: {} = {}".format(self._key, self._new_value))
+        self._dictionary._realAddItem(self._key, self._new_value)
+
+
+class _SetItemCommand(_EmptyCommand):
+    """
+    The _SetItemCommand class implements a command to modify the value of
+    the existing key in the URDict-based dictionary.
+    """
+
+    def undo(self) -> NoReturn:
+        self.setText("Setting: {} = {}".format(self._key, self._old_value))
+        self._dictionary._realSetItem(self._key, self._old_value)
+
+    def redo(self) -> NoReturn:
+        self.setText("Setting: {} = {}".format(self._key, self._new_value))
+        self._dictionary._realSetItem(self._key, self._new_value)
+
+
+class URDict(dict):
+    """
+    The URDict class implements a dictionary-based class with undo/redo
+    functionality based on QUndoStack.
+    """
+
+    # URDict constructor
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._stack = QUndoStack()
+
+    # Private URDict dictionary-based methods to be called via the QUndoCommand-based classes.
+
+    def _realSetItem(self, key: Union[str, List], value: Any) -> NoReturn:
+        """Actually changes the value for the existing key in dictionary."""
+        if isinstance(key, list):
+            self.getItemByPath(key[:-1])[key[-1]] = value
+        else:
+            super().__setitem__(key, value)
+
+    def _realAddItem(self, key: str, value: Any) -> NoReturn:
+        """Actually adds a key-value pair to dictionary."""
+        super().__setitem__(key, value)
+
+    def _realDelItem(self, key: str) -> NoReturn:
+        """Actually deletes a key-value pair from dictionary."""
+        del self[key]
+
+    def _realSetItemByPath(self, keys: List, value: Any) -> NoReturn:
+        """Actually sets the value in a nested object by the key sequence."""
+        self.getItemByPath(keys[:-1])[keys[-1]] = value
+
+    # Public URDict dictionary-based methods
+
+    def __setitem__(self, key: str, val: Any) -> NoReturn:
+        """Overrides default dictionary assignment to self[key] implementation.
+        Calls the undoable command and pushes this command on the stack."""
+        if key in self:
+            self._stack.push(_SetItemCommand(self, key, val))
+        else:
+            self._stack.push(_AddItemCommand(self, key, val))
+
+    def __getitem__(self, key: str) -> Any:
+        """Overrides default evaluation of self[key] implementation."""
+        return super().__getitem__(key)
+
+    def setItemByPath(self, keys: List, value: Any) -> NoReturn:
+        """Calls the undoable command to set a value in a nested object
+        by key sequence and pushes this command on the stack."""
+        self._stack.push(_SetItemCommand(self, keys, value))
+
+    def getItemByPath(self, keys: List, default=None) -> Any:
+        """Returns a value in a nested object by key sequence."""
+        item = self
+        for key in keys:
+            if key in item.keys():
+                item = item[key]
+            else:
+                return default
+        return item
+
+    def getItem(self, key: Union[str, List], default=None):
+        """Returns a value in a nested object. Key can be either a sequence
+        or a simple string."""
+        if isinstance(key, list):
+            return self.getItemByPath(key, default)
+        else:
+            return self.get(key, default)
+
+    # Public URDict undostack-based methods
+
+    def undoText(self) -> NoReturn:
+        """Returns the text of the command which will be undone in the next
+        call to undo()."""
+        return self._stack.undoText()
+
+    def redoText(self) -> NoReturn:
+        """Returns the text of the command which will be redone in the next
+        call to redo()."""
+        return self._stack.redoText()
+
+    def undo(self) -> NoReturn:
+        """Undoes the current command on stack."""
+        print("\nUndo command is called")
+        self._stack.undo()
+
+    def redo(self) -> NoReturn:
+        """Redoes the current command on stack."""
+        print("\nRedo command is called")
+        self._stack.redo()
---
+++ DictUndoRedoDecorator2/main.py
@@ -0,0 +1,118 @@
+from URDict import URDict
+
+
+if __name__ == '__main__':
+
+    print("\n****Test phase 1****")
+
+    test = URDict(a="AAA", b="BBB")
+    print("\n-1 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test['c'] = 'CCC'
+    print("\n-2 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test['d'] = 'DDD'
+    print("\n-3 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-4 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-5 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.redo()
+    print("\n-6 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test['a'] = '---'
+    print("\n-7 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-8 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.redo()
+    print("\n-9 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test['b'] = '---'
+    print("\n-10 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-11 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.redo()
+    print("\n-12 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    print("\n****Test phase 2****")
+
+    test = URDict(dict(a=1, b=2, c=dict(d=3, e=dict(f=4, g=5))))
+    print("\n-1 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test['a'] = "---"
+    print("\n-2 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-3 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.redo()
+    print("\n-4 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    #test['c']['e']['g'] = "999"
+    #print("\n-5 ", test)
+    #print("Undo", test.undoText())
+    #print("Redo", test.redoText())
+
+    #test.undo()
+    #print("\n-6 ", test)
+    #print("Undo", test.undoText())
+    #print("Redo", test.redoText())
+
+    #test.redo()
+    #print("\n-7 ", test)
+    #print("Undo", test.undoText())
+    #print("Redo", test.redoText())
+
+    test.setItemByPath(['c', 'e', 'g'], '***')
+    print("\n-8 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.undo()
+    print("\n-9 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
+
+    test.redo()
+    print("\n-10 ", test)
+    print("Undo", test.undoText())
+    print("Redo", test.redoText())
