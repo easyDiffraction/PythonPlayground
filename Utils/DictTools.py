4be828852b058bdb32094d0981597a37ddb85342
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-01-22 23:43:31 +0100

    Add comparison and tests

--- Utils/DictTools.py
+++ Utils/DictTools.py
@@ -1,4 +1,4 @@
-from typing import Union, Any, NoReturn, List
+from typing import Union, Any, NoReturn, List, Iterable, Tuple
 from collections import UserDict
 from copy import deepcopy
 
@@ -127,6 +127,70 @@ class PathDict(UserDict):
                 baseD[key] = item.toBase()
         return baseD
 
+    def dictComparison(self, new_dict: Union['PathDict', dict]) -> Tuple[list, list]:
+        """
+        Compare self to a dictionary or PathDict and return the update path and value
+        :param new_dict: dict or PathDict to compare self to
+        :return: path and value updates for self to become newDict
+        """
+        def dictIterator(old_dict_in: dict, new_dict_in: dict) -> Tuple[list, list]:
+            """
+            Iterate through a dict and find all comparisons
+            :param old_dict_in: Base dictionary
+            :param new_dict_in: dictionary to compare to base dictionary
+            :return: list of update locations and list of update values
+            """
+            keyList = []
+            itemList = []
+            for key, item in new_dict_in.items():
+                if key not in old_dict_in.keys():
+                    # The field does not exist in the old dict
+                    keyList.append(key)
+                    itemList.append(item)
+                else:
+                    # The key exists in both dicts
+                    tempItem = old_dict_in[key]
+                    if isinstance(item, dict):
+                        # Its a dictionary, so we have to call `dictComparison` again
+                        nestedKeyList, nestedItemList = dictIterator(tempItem, item)
+                        if len(nestedKeyList) > 0:
+                            keyList.append([key, nestedKeyList])
+                            itemList.append(nestedItemList)
+                    else:
+                        # We know that we're left with objects and numbers strings etc
+                        if item is not tempItem:
+                            # They are not the same. Boo
+                            keyList.append(key)
+                            itemList.append(item)
+            return keyList, itemList
+
+        def prettyKey(keylist: list) -> list:
+            """
+            Makes the key list into a UndoableDict path
+            """
+            for i, key in enumerate(keylist):
+                if isinstance(key, list):
+                    if len(key) == 1:
+                        keylist[i] = key[0]
+                    else:
+                        keylist[i] = prettyKey(key)
+            return keylist
+
+        def flatten(items: list) -> list:
+            """
+            Yield items from any nested iterable
+            """
+            for item in items:
+                if isinstance(item, Iterable) and not isinstance(item, (str, bytes)):
+                    for sub_x in flatten(item):
+                        yield sub_x
+                else:
+                    yield item
+        if isinstance(new_dict, PathDict):
+            new_dict = new_dict.toDict()
+        keyList, itemList = dictIterator(self.toDict(), new_dict)
+        return prettyKey(keyList), list(flatten(itemList))
+
 
 class UndoableDict(PathDict):
     """
@@ -142,28 +206,50 @@ class UndoableDict(PathDict):
     # Public methods
 
     def __setitem__(self, key: str, val: Any) -> NoReturn:
-        """Calls the undoable command to override PathDict assignment to self[key]
-        implementation and pushes this command on the stack."""
+        """
+        Calls the undoable command to override PathDict assignment to self[key]
+        implementation and pushes this command on the stack.
+        """
         if key in self:
             self.__stack.push(_SetItemCommand(self, key, val))
         else:
             self.__stack.push(_AddItemCommand(self, key, val))
 
     def setItemByPath(self, keys: list, value: Any) -> NoReturn:
-        """Calls the undoable command to set a value in a nested object
-        by key sequence and pushes this command on the stack."""
+        """
+        Calls the undoable command to set a value in a nested object
+        by key sequence and pushes this command on the stack.
+        """
         self.__stack.push(_SetItemCommand(self, keys, value))
 
     def undo(self) -> NoReturn:
-        """Undoes the current command on stack."""
+        """
+        Undoes the current command on stack.
+        """
         self.__stack.undo()
 
     def redo(self) -> NoReturn:
-        """Redoes the current command on stack."""
+        """
+        Redoes the current command on stack.
+        """
         self.__stack.redo()
 
+    def undoText(self) -> str:
+        """
+        :return the current command on stack.
+        """
+        return self.__stack.undoText()
+
+    def redoText(self) -> str:
+        """
+        :return the current command on stack.
+        """
+        return self.__stack.redoText()
+
     def startBulkUpdate(self, text='Bulk update') -> NoReturn:
-        """Begins composition of a macro command with the given text description."""
+        """
+        Begins composition of a macro command with the given text description.
+        """
         if self._macroRunning:
             print('Macro already running')
             return
@@ -171,9 +257,23 @@ class UndoableDict(PathDict):
         self._macroRunning = True
 
     def endBulkUpdate(self) -> NoReturn:
-        """Ends composition of a macro command."""
+        """
+        Ends composition of a macro command.
+        """
         if not self._macroRunning:
             print('Macro not running')
             return
         self.__stack.endMacro()
         self._macroRunning = False
+
+    def bulkUpdate(self, key_list: list, item_list: list) -> NoReturn:
+        """
+        Performs a bulk update based on a list of keys and a list of values
+        :param key_list: list of keys or path keys to be updated
+        :param item_list: the value to be updated
+        :return: None
+        """
+        self.startBulkUpdate()
+        for key, value in zip(key_list, item_list):
+            self.setItemByPath(key, value)
+        self.endBulkUpdate()
